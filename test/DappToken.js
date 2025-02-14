994b603206a398bd1fe1cfadf4596fb5a26a35da
siddhit-kale <ksiddhit9@gmail.com>
2025-02-14 15:44:50 +0530

    Transfer token

--- test/DappToken.js
+++ test/DappToken.js
@@ -37,7 +37,7 @@ contract('DappToken', function(accounts) {
       return tokenInstance.transfer.call(accounts[1], 1000000000);
     }).then(assert.fail).catch(function(error) {
       assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
-      return tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] });
+      return tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] }); 
     }).then(function(success) {
       assert.equal(success, true, 'it returns true');
       return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] });
